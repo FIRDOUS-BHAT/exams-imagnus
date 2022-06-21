20d1519f0f7b17737320d52628b4a928acab49a8
FIRDOUS <frdsbhat9@gmail.com>
2022-06-21 16:27:03 +0530

    added subscription plans validation on web

--- FCM/route.py
+++ FCM/route.py
@@ -1,11 +1,18 @@
 # Send to single device.
+from fastapi import APIRouter
 from pyfcm import FCMNotification
+from fastapi.responses import JSONResponse
+
+from student.models import Student
+
+router = APIRouter()
 
 
 api_key = "REDACTED_FCM_SERVER_KEY"
 push_service = FCMNotification(api_key=api_key)
 
-registration_ids= ["f7fHfPD0Q0eFavzB_yp4NE:REDACTED_FCM_TOKEN",]
+registration_ids = [
+    "f7fHfPD0Q0eFavzB_yp4NE:REDACTED_FCM_TOKEN", ]
 # Sending a notification with data message payload
 data_message = {}
 
@@ -19,8 +26,8 @@ registration_id = "fL_mFnfASkuUQ72Yr2GSUq:REDACTED_FCM_TOKEN"
 # message_title = 'Payment successful'
 
 # message_body = "You have successfully purchased the "+name+ " course.\n Happy Learning "
-# result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)   
-                                
+# result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+
 
 # print(result)
 # # To multiple devices
@@ -30,3 +37,33 @@ registration_id = "fL_mFnfASkuUQ72Yr2GSUq:REDACTED_FCM_TOKEN"
 #     data_message=data_message
 # )
 
+
+@router.post('/send_app_update_notification/')
+async def send_app_update_notification():
+    try:
+        fcm_list = await Student.all().values("fcm_token")
+        fcms = list(map(lambda x: x['fcm_token'], fcm_list))
+        registration_ids = list(filter(None, fcms))
+        registration_ids = [
+            "dxjNGZ5vT6WuDe4e5EScgE:REDACTED_FCM_TOKEN"]
+
+        message_title = "imagnus app update available"
+
+        extra_notification_kwargs = {
+            "open": "external_url",
+            "data_payload": "https://play.google.com/store/apps/details?id=com.smcln.imagnus"
+        }
+        data_message = "Dear student, \n\n There is an app update available. Please tap here to go to open the  app link. Ignore this notification if you already updated app to the newest version."
+        result = push_service.notify_multiple_devices(
+            registration_ids=registration_ids,
+            message_icon="app_icon",
+            message_title=message_title,
+            message_body=data_message,
+            click_action="FLUTTER_NOTIFICATION_CLICK",
+            data_message=extra_notification_kwargs,
+        )
+
+        return JSONResponse({"status": True, "message": "Notifications sent."})
+
+    except Exception as ex:
+        return JSONResponse({"status": False, "message": str(ex)})
--- apis/routes.py
+++ apis/routes.py
@@ -5,8 +5,11 @@ from admin_dashboard.apis import route as preferenceRoute
 from send_sms import api
 from checkout.apis import route as checkoutRoute
 from study_material.apis import route as studyMaterialRoute
+from FCM import route as FcmRoute
+
 
 api_router = APIRouter()
+# api_router.include_router(FcmRoute, prefix='/notify')
 api_router.include_router(bannerRoute.router, )
 api_router.include_router(studentRoute.router, prefix='/student')
 api_router.include_router(preferenceRoute.router, prefix='/student')
