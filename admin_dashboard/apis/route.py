2001a08443fb3cea798c4bbbea25736db1c9eaf8
FIRDOUS <frdsbhat9@gmail.com>
2022-11-20 12:18:46 +0530

    test..

--- admin_dashboard/apis/route.py
+++ admin_dashboard/apis/route.py
@@ -1316,9 +1316,9 @@ async def course_category(course_slug: str, category_slug: str, student_id: str,
                                 for j in range(remaining_counter):
                                     updated_dict = category_topics_obj.CategoryTestSeries[
                                         subscription_series_counter + j].dict(exclude={'CategoryTestSeriesQuestions'})
-                                    test_series_id = None
-                                    # test_series_id = category_topics_obj.CategoryTestSeries[
-                                    #     subscription_series_counter + j].id
+                                    # test_series_id = None
+                                    test_series_id = category_topics_obj.CategoryTestSeries[
+                                        subscription_series_counter + j].id
                                     is_bookmarked = await check_isBookmarkedTestSeries(test_series_id)
                                     updated_dict.update(
                                         {"isBookmarked": is_bookmarked})
@@ -1442,7 +1442,7 @@ async def course_category(course_slug: str, category_slug: str, student_id: str,
                             }
 
                             return result
-                        else:
+                else:
                             '''Free content free tier'''
 
                             initial_video_counter = 2
