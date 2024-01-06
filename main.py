1181d2f6b6f8bfa0064ce7b67e75954f89eb6f18
FIRDOUS-BHAT <frdsbhat9@gmail.com>
2024-01-06 13:59:15 +0530

    new logger

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
+import json
 
 
 
@@ -124,19 +125,27 @@ app.lifespan = lifespan  # Assign the lifespan function
 
 @app.exception_handler(Exception)
 async def global_exception_handler(request, exc):
-    # Log the error
-    print('error occured')
+    # Construct a log entry as a dictionary
+    log_entry = {
+        "timestamp": datetime.now().isoformat(),
+        "level": "ERROR",
+        "message": "An error occurred",
+        "api_endpoint": request.url.path,
+        "http_method": request.method,
+        "error": str(exc),
+    }
 
-    # Retrieve the API endpoint from the request object
-    api_endpoint = request.url.path
+    # Log the structured error message with pretty print
+    formatted_log = json.dumps(log_entry, indent=2)
+    logger.error(formatted_log)
 
-    logger.error(f"An error occurred in API '{api_endpoint}': {exc}")
+    # Print the error for debugging (optional)
+    # print(formatted_log)
+
     return JSONResponse(
         status_code=500,
         content={"message": "Internal Server Error"},
     )
-
-    
   
 
 
