2fcafaabf8aac66c3fc468d3b84b20bbce01b82e
FIRDOUS-BHAT <frdsbhat9@gmail.com>
2024-02-19 19:23:24 +0530

    added validations in order webhook

--- checkout/apis/route.py
+++ checkout/apis/route.py
@@ -556,7 +556,7 @@ class OrderPlacePydantic(BaseModel):
 async def place_order(data: OrderPlacePydantic, _=Depends(get_current_user)):
     # Check and process the regular order
     try:
-        if data.payment_id and not data.order_id:
+        if data.source != "adm" and data.payment_id and not data.order_id:
             return JSONResponse(
                 {"status": False, "message": "Invalid Order Id"}, status_code=208
             )
